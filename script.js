9ee9636615d73ee6c4eb14e3178b629bb4efe1df
noorulmobin <noorulmobin0011@gmail.com>
2024-05-17 01:12:38 +0500

    second changes

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-function toggleDropdown() {
-  var dropdown = document.getElementById("list");
-  dropdown.classList.toggle("show");
-}
-
-// Close the dropdown when clicking outside of it
-window.addEventListener("click", function (event) {
-  var dropdown = document.getElementById("list");
-  var menuIcon = document.querySelector(".menu-icon");
-  if (event.target !== menuIcon && !dropdown.contains(event.target)) {
-    dropdown.classList.remove("show");
+function toggleMenu() {
+    const navItems = document.querySelector('.nav-items');
+    if (navItems.style.display === 'block') {
+      navItems.style.display = 'none';
+    } else {
+      navItems.style.display = 'block';
+    }
   }
-});
